b3891cdb9040900246f85fb33b29d358acbcf5c2
Niko Rivas <rivasniko@gmail.com>
2022-06-23 19:30:23 -0700

    make filetree and test scripts

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+function wordCounter(text) {
+  if (text.length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const wordArray = text.split(' ');
+  wordArray.forEach(function (word) {
+    wordCount++;
+  });
+  return wordCount;
+}
+
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const wordArray = text.split(' ');
+  wordArray.forEach(function (element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+function numberOfOccurrencesInText(word, text) {
+  return 0;
+}
+
+function numberOfOccurrencesInText(word, text) {
+  const wordArray = text.split(' ');
+  let wordCount = 0;
+  wordArray.forEach(function (element) {
+    if (word === element) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
