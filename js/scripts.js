8ef19277e7b16c82664b4326ddee4599875463cf
Niko Rivas <rivasniko@gmail.com>
2022-06-26 11:33:59 -0700

    add tests to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) {
+  if (text.trim().length === 0 || word.trim().length === 0) {
     return 0;
   }
   const wordArray = text.split(' ');
@@ -29,3 +29,39 @@ function numberOfOccurrencesInText(word, text) {
 }
 
 // UI Logic
+function handleFormSubmission() {
+  event.preventDefault();
+  const passage = document.getElementById('text-passage').value;
+  const word = document.getElementById('word').value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById('total-count').innerText = wordCount;
+  document.getElementById('selected-count').innerText = occurrencesOfWord;
+}
+
+window.addEventListener('load', function () {
+  document
+    .querySelector('form#word-counter')
+    .addEventListener('submit', handleFormSubmission);
+});
+
+function boldPassage(word, text) {
+  if (text.trim().length === 0 || word.trim().length === 0) {
+    return null;
+  }
+  const p = document.createElement('p');
+  let textArray = text.split(' ');
+  textArray.forEach(function (element) {
+    if (word === element) {
+      const bold = document.createElement('strong');
+      bold.append(element);
+      p.append(bold);
+    } else {
+      p.append(element);
+    }
+    if (index !== textArray.length - 1) {
+      p.append(' ');
+    }
+  });
+  return p;
+}
