7b00f9573dad4453091dd4120b2bff13fd7086c4
Niko Rivas <rivasniko@gmail.com>
2022-06-26 09:54:46 -0700

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,4 @@
-function wordCounter(text) {
-  if (text.length === 0) {
-    return 0;
-  }
-  let wordCount = 0;
-  const wordArray = text.split(' ');
-  wordArray.forEach(function (word) {
-    wordCount++;
-  });
-  return wordCount;
-}
+// Business Logic
 
 function wordCounter(text) {
   if (text.trim().length === 0) {
@@ -18,37 +8,6 @@ function wordCounter(text) {
   const wordArray = text.split(' ');
   wordArray.forEach(function (element) {
     if (!Number(element)) {
-      wordCount++;
-    }
-  });
-  return wordCount;
-}
-function numberOfOccurrencesInText(word, text) {
-  return 0;
-}
-
-function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) {
-    return 0;
-  }
-  const wordArray = text.split(' ');
-  let wordCount = 0;
-  wordArray.forEach(function (element) {
-    if (word === element) {
-      wordCount++;
-    }
-  });
-  return wordCount;
-}
-
-function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) {
-    return 0;
-  }
-  const wordArray = text.split(' ');
-  let wordCount = 0;
-  wordArray.forEach(function (element) {
-    if (word.toLowerCase() === element.toLowerCase()) {
       wordCount++;
     }
   });
@@ -69,4 +28,4 @@ function numberOfOccurrencesInText(word, text) {
   return wordCount;
 }
 
-function removeOffensiveText(text) {}
+// UI Logic
